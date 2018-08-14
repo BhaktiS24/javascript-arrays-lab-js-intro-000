73342d63cf43734d1d4f6993bf497f67b935af69
Bhakti Sawarkar <bhakti123.java@gmail.com>
2018-08-14 17:09:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name)
+{
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name)
+{
+  kittens.shift(name)
+}
+
+function appendKitten(name)
+{
+  return [name, ...kittens]
+}
+
+function prependKitten(name)
+{
+  return[...kitten,name]
+}
+
+function removeLastKitten()
+{
+  return kittens.slice(0,kittens.lenght - 1)
+}
+
+function removeFirstKitten()
+{
+  return kittens.slice(1)
+}
